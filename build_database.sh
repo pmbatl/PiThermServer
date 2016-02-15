8c1ad9c6360f6d793bc18f3d7ed9921c2ac3a7c9
pmbatl <peter.bethany@gmail.com>
2016-02-15 09:38:16 -0500

    Updated with date

--- build_database.sh
+++ build_database.sh
@@ -3,7 +3,7 @@
 # build_database.sh - create empty temperature database schema for to log temperature in.
 #
 # Tom Holderness 22/01/2012
-# Modified by Peter Bethany to support multiple temperature sensors
+# Modified by Peter Bethany to support multiple temperature sensors 02/15/2016
 #
 sqlite3 piTemps.db 'DROP TABLE temperature_records;'
 sqlite3 piTemps.db 'CREATE TABLE temperature_records(unix_time bigint primary key, therm1 real, therm2 real, therm3 real);'
